404aa03f294276e48a2e8fd34472b834299c61c4
Tirth Teraiya <iamtirth0405@gmail.coma>
2025-12-30 12:13:40 +0530

    Add sotrybook configuration

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  // Tells Storybook where to find your story files
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
